90c47a24b0148e2982bd28747a47a5d25705f217
AkhmaevM <m.axmaev@yandex.ru>
2026-05-05 00:55:25 +0300

    Added a fetch request to the free API using the value entered by the user and hidden the HTML code with placeholder movie cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,17 @@
 const userInput = document.querySelector('.movies__input');
 const searchBtn = document.querySelector('.movies__search');
 
-searchBtn.addEventListener('click', ()=>{
-    if(userInput.value === ''){
+searchBtn.addEventListener('click', async () => {
+    if (userInput.value === '') {
         alert('Empty input');
     }
 
-    console.log(userInput.value);
+
+    let response = await fetch(`http://www.omdbapi.com/?apikey=b83f90d7&t=${userInput.value}`);
+
+    let text = await response.text(); // прочитать тело ответа как текст
+
+    console.log(text.slice(0, 80) + '...');
+    console.log(text);
 })
 
